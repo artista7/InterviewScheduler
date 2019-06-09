a22035f61babb7d7ebad4b94867ef9dca0a7286a
vivekverma239 <vivekverma239@gmail.com>
2019-06-09 15:32:25 +0530

    Changed APP File Name

--- application.py
+++ application.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify, request
 
 from optimization  import optimize_schedule
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
-def hello():
+@application.route('/optimize')
+def optimize():
     schedule_data = request.get_json()
     optimized_schedule = optimize_schedule(schedule_data)
     return jsonify(optimized_schedule)
 
+@application.route('/')
+def hello():
+    return 'Hello, World!'
+
 if __name__ == '__main__':
-    app.run()
+    application.run(host='0.0.0.0')

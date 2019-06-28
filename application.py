6938d9b7c56ea1157327013e011bdcebf92e431a
vivekverma239 <vivekverma239@gmail.com>
2019-06-29 00:36:58 +0530

    Added PUT in optimize

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ from optimization  import optimize_schedule
 application = Flask(__name__)
 
 
-@application.route('/optimize')
+@application.route('/optimize', methods=['GET', 'PUT'])
 def optimize():
     schedule_data = request.get_json()
     optimized_schedule = optimize_schedule(schedule_data)
@@ -13,6 +13,7 @@ def optimize():
 @application.route('/')
 def hello():
     return 'Hello, World!'
+
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')

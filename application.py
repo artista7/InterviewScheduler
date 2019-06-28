55861c32108c20ff95d18693934f8a37c662b78a
vivekverma239 <vivekverma239@gmail.com>
2019-06-29 01:15:10 +0530

    Added flask-cors to allow flask-cors

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 
 from optimization  import optimize_schedule
 application = Flask(__name__)
+cors = CORS(application)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 
 @application.route('/optimize', methods=['GET', 'PUT'])
+@cross_origin()
 def optimize():
     schedule_data = request.get_json()
     optimized_schedule = optimize_schedule(schedule_data)

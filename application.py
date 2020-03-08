c19f7220f4bc59fe01a5b73e4e4b5e0e3a03c308
Vivek Verma <vivekverma239@gmail.com>
2020-03-09 00:30:42 +0530

    change application file

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify, request
+import os
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 
 from optimization  import optimize_schedule
 
-application = Flask(__name__)
+application = Flask(__name__, static_folder='ui/build')
 CORS(application)
 
 
@@ -12,6 +13,16 @@ def optimize():
     schedule_data = request.get_json()
     optimized_schedule = optimize_schedule(schedule_data)
     return jsonify(optimized_schedule)
+
+
+# Serve React App
+@application.route('/', defaults={'path': ''})
+@application.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(application.static_folder + '/' + path):
+        return send_from_directory(application.static_folder, path)
+    else:
+        return send_from_directory(application.static_folder, 'index.html')
 
 
 if __name__ == '__main__':
